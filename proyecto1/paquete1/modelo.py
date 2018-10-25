46efdb333c63fa177949392b71c8ca48e1781014
David <jhandry13@hotmail.com>
2018-10-24 23:59:55 -0500

    Cambiando linea de for a una mas optima

--- proyecto1/paquete1/modelo.py
+++ proyecto1/paquete1/modelo.py
@@ -18,14 +18,16 @@ class Docente:
 	def obtener_ciudad(self):
 		return self.ciudad
 
+	#se crea el metodo para obtener la presentacion de datos 
 	def presentar_datos(self):
 		cadena = "%s\n\t%s" % (self.obtener_nombres(), self.obtener_ciudad())
 		return cadena
-#se crea clase 
+#se crea clase estudiante
 class Estudiante:
 	def __init__(self,n,lista_docentes):
 		self.nombres = n
 		self.docentes = lista_docentes
+	
 	# metodos para obtener y agregar nombre
 	def agregar_nombre(self,n):
 		self.nombres = n
@@ -39,10 +41,12 @@ class Estudiante:
 
 	def obtener_docentes(self):
 		return self.docentes
-
+    
+    #se crea metodo para la presentacion de datos
 	def presentar_datos(self):
-		cadena = "Estudiante: %s\n" % (self.obtener_nombres())
-		cadena = "%s %s\n" % (cadena,"lista_docentes")
-		for x in range(0, len(self.docentes)):
-			cadena = "%s\n\t-%s|%s" %(cadena, self.docentes[x].obtener_nombres(), self.docentes[x].obtener_ciudad())
+		cadena = "Estudiante:%s\n" % (self.obtener_nombres())
+		cadena = "%s%s\n" % (cadena,"Lista_docentes")#se concatena la primera cadena a una nueva que tenga el encabezado de las listas
+		#se recorre cada valor de la lista que devuelbe docentes
+		for x in self.obtener_docentes():
+			cadena = "%s %s:\n " %(cadena, x.presentar_datos()) #se concatena a la cadena la lista que retorna docentes
 		return cadena		
--- proyecto1/principal.py
+++ proyecto1/principal.py
@@ -1,8 +1,8 @@
 #se importa los metodos del paquete 1
 from paquete1.modelo import *
 #se crea los objetos 
-d = Docente("Docente B.D", "Loja")
-d2 = Docente("Docente Pray", "Quito")
+d = Docente("Docente: B.D", "Loja")
+d2 = Docente("Docente: Pray", "Quito")
 #se crea la lista 
 listado = [d,d2]
 e = Estudiante("Luis", listado)
